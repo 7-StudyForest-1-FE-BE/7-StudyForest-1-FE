f323dc998e5b81be8794ffb229e256e5e33d426b
dslee0525 <lds8835@gmail.com>
2025-06-12 19:40:59 +0900

    feat: 공부의숲 홈, 마크업 및 스타일

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -7,17 +7,17 @@ function Header() {
       <div className={styles.inner}>
         <div className={styles.logo}>
           <h1>
-            <span className="sr-only">공부의숲</span>
+            <span className="sr__only">공부의숲</span>
             <Link to={"/"}>
               <img src={logo} alt="공부의숲" />
             </Link>
           </h1>
         </div>
-        {/*<div className={styles.util}>
+        <div className={styles.util}>
           <Link to={"/registration"} className={styles.btn}>
             스터디 만들기
           </Link>
-        </div>*/}
+        </div>
       </div>
     </header>
   );
