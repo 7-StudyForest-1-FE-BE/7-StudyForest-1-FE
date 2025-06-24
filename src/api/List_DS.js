56afc86438d4f02c235d272f77453c3063018322
SeongGeun16 <seong00016@gmail.com>
2025-06-24 14:41:00 +0900

    api 파일 복구

---
+++ src/api/List_DS.js
@@ -0,0 +1,48 @@
+export const getStudyList = async ({ offset = 0, limit = 6 }) => {
+  const response = await fetch(
+    `http://localhost:3000/api/studies?offset=${offset}&limit=${limit}`
+  );
+  if (!response.ok) throw new Error("스터디 목록 불러오기 실패");
+  const data = await response.json();
+  return data;
+};
+
+export const getStudyItem = async (studyId) => {
+  const response = await fetch(
+    `http://localhost:3000/api/studies/${studyId}?populateHabits=true`
+  );
+  if (!response.ok) throw new Error("개별 스터디 상세보기 실패");
+  const data = await response.json();
+  return data;
+};
+
+export const checkStudyPassword = async (studyId, password) => {
+  const res = await fetch(
+    `http://localhost:3000/api/studies/${studyId}/check-password`,
+    {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ password }),
+    }
+  );
+
+  if (!res.ok) {
+    let message = "비밀번호 확인 실패";
+    throw new Error(message);
+  }
+
+  return await res.json();
+};
+
+export const getStudyHabits = async (studyId) => {
+  const response = await fetch(
+    `http://localhost:3000/api/studies/${studyId}?populateHabits=true`
+  );
+  if (!response.ok) throw new Error("스터디 습관 불러오기 실패");
+
+  const data = await response.json();
+
+  return data.habits.map((habit) => habit.title);
+};
