6998efc18a8d667bbcab7134bb4a6e8d0d470182
aha-hyeong <ahahyeong@gmail.com>
2025-06-26 13:07:58 +0900

    input, textarea의 placeholder 폰트 문제 수정

--- src/components/Registation/BackgroundSelector.jsx
+++ src/components/Registation/BackgroundSelector.jsx
@@ -1,32 +1,28 @@
-import React from "react";
-import styles from "./BackgroundSelector.module.css";
-import pawIcon from "../../assets/sticker/gray_bg_selected.svg";
-import bgThemes from "../../data/bgThemes";
+import React from 'react';
+import styles from './BackgroundSelector.module.css';
+import pawIcon from '../../assets/sticker/gray_bg_selected.svg';
+import bgThemes from '../../data/bgThemes';
 
 function BackgroundSelector({ selectedBgId, onSelect }) {
   return (
     <div className={styles.choice__background}>
       <label className={styles.label}>배경을 선택해주세요</label>
       <div className={styles.background__items}>
-        {bgThemes.map((bg, idx) => (
+        {bgThemes.map((bg) => (
           <button
-            key={idx}
+            key={bg.id}
             type="button"
-            className={`${styles.background__item} ${
-              selectedBgId === idx + 1 ? styles.selected : ""
-            }`}
+            className={`${styles.background__item} ${selectedBgId === bg.id ? styles.selected : ''}`}
             style={
-              bg.type === "color"
+              bg.type === 'color'
                 ? { background: bg.value }
-                : bg.type === "image"
+                : bg.type === 'image'
                 ? { backgroundImage: `url(${bg.value})` }
                 : {}
             }
-            onClick={() => onSelect(idx + 1)}
+            onClick={() => onSelect(bg.id)}
           >
-            {selectedBgId === idx + 1 && (
-              <img src={pawIcon} alt="선택됨" className={styles.paw__icon} />
-            )}
+            {selectedBgId === bg.id && <img src={pawIcon} alt="선택됨" className={styles.paw__icon} />}
           </button>
         ))}
       </div>
